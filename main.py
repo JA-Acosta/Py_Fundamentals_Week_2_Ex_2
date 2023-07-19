6b10ae2a6096ef78e972cac36e71a1b7e77fb4fd
JAAR <joe.aaron.acosta@outlook.com>
2023-07-19 00:28:56 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/19/2023
+>>> Practicing Fundamentals Program 8
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that will print the multiplication table for a number and will then print the factorial for the number. Afterwards, the program will generate the sum for each pair.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
